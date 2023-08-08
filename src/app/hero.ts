71fe4f8204e1fc60e720f02b45b79a649cf2c3f7
Brayan <brayantandap@gmail.com>
2023-08-08 12:27:50 -0500

    chore: Update comments and improve code readability
    - Updated comments in various sections of the code for better clarity and added more descriptive explanations. Also, improved code readability by reformatting indentation and ensuring consistent naming conventions.

--- src/app/hero.ts
+++ src/app/hero.ts
@@ -1,3 +1,4 @@
+// Interface to define  data type and their name in the heroe's array
 export interface Hero {
   id: number;
   name: string;
--- src/app/message.service.ts
+++ src/app/message.service.ts
@@ -1,15 +1,24 @@
+// Import the 'Intectable' function from the library '@angular/core'
 import { Injectable } from '@angular/core';
 
+// Decorator makes the service availble for dependency injection throughot the entire app.
 @Injectable({
+  // Provided at the root level of the application, creating a single instance of the service.
   providedIn: 'root',
 })
+
+
+// This class manages the service. It's initialized with an empty string array.
+// it defines two methods: one for displaying a message with an 'ID',
+// and another for clearing or deleting the selected messages.
 export class MessageService {
   messages: string[] = [];
 
+  // method to add a new message to the messages array.
   add(message: string) {
     this.messages.push(message);
   }
-
+  // method to clear all messages from the messages array.
   clear() {
     this.messages = [];
   }
